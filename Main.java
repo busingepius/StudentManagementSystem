8749cd8ed1b34278fd951d21a6a8f3cda75119b2
busingepius <busingepius@gmail.com>
2024-03-15 16:17:37 -0500

    1.0

---
+++ Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created by: Pius and Phone
+ * Company : Gegabox
+ * Year : 2024
+ * Date : 15/03/2024
+ * Time : 16:14
+ * Project Name : PACKAGE_NAME
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
